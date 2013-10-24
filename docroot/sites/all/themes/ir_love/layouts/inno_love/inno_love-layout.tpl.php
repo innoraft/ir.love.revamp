2557c429eef8c27b3d152c8a1af6062860573d5d
SGupta14 <somya.gupta@innoraft.com>
2013-10-24 16:24:33 +0530

    Custom layout tpl changes
    * added a new region
    * added div for main idea font icons
    
    reference innoraft/ir.love.revamp#10

--- docroot/sites/all/themes/ir_love/layouts/inno_love/inno_love-layout.tpl.php
+++ docroot/sites/all/themes/ir_love/layouts/inno_love/inno_love-layout.tpl.php
@@ -7,6 +7,12 @@
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
       <?php endif; ?>
+      <div class="ir_custom_logo">
+        <div class="ir-custom-logo-icons">
+          <div id="ir-drupal" class="ir-main-idea"><i class="fa fa-cog"></i></div>
+          <div id="ir-love" class="ir-main-idea"><i class="fa fa-heart"></i></div>
+          <div id="ir-dedication" class="ir-main-idea"><i class="fa fa-trophy"></i></div>
+        </div>
       <?php if ($site_name || $site_slogan): ?>
         <?php if ($site_name): ?>
           <h1 class="site-name">
@@ -18,12 +24,13 @@
           <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
       <?php endif; ?>
+    </div>
 
       <?php print render($page['branding']); ?>
     </div>
 
-    <?php print render($page['header']); ?>
-    <?php print render($page['navigation']); ?>
+    <?php //print render($page['header']); ?>
+    <?php //print render($page['navigation']); ?>
   </header>
 
   <div class="l-main">
