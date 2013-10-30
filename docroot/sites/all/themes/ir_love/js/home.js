44e0f0784ea7765067d4298e86daa4ff1070fa75
Sumeet Pareek <sumeet.pareek@innoraft.com>
2013-10-30 19:21:03 +0530

    removed everything from home.js

--- docroot/sites/all/themes/ir_love/js/home.js
+++ docroot/sites/all/themes/ir_love/js/home.js
@@ -1,11 +1,4 @@
 (function ($) {
 	$(document).ready(function(){
-		if (screen.width > 769) {
-			var slide_out = $('.ir-idea-font-icon');
-			var slide_in = $('.ir-idea-text');
-
-			$('.ir-main-idea').height(slide_in.height());
-			//$('.ir-idea-text').hide();
-		}
 	});
 }(jQuery));
