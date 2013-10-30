859322e128d559ecb247cbdb094b4d307208582a
Sumeet Pareek <sumeet.pareek@innoraft.com>
2013-10-30 19:11:43 +0530

    animation removed

--- docroot/sites/all/themes/ir_love/js/home.js
+++ docroot/sites/all/themes/ir_love/js/home.js
@@ -1,7 +1,6 @@
 (function ($) {
 	$(document).ready(function(){
 		if (screen.width > 769) {
-			ir_logo_effect();
 			var slide_out = $('.ir-idea-font-icon');
 			var slide_in = $('.ir-idea-text');
 
@@ -9,18 +8,4 @@
 			//$('.ir-idea-text').hide();
 		}
 	});
-	function ir_logo_effect() {
-			$('.ir-logo').mouseenter(function(){
-				//alert("site logo");
-				$('.ir-idea-font-icon').toggle("drop", 1000);
-				$('.ir-idea-text').toggle("drop", 1000);
-			});
-			$('.ir-logo').mouseleave(function(){
-				$('.ir-idea-font-icon').toggle("drop", 1000);
-				$('.ir-idea-text').toggle("drop", 1000);
-			});
-		}
-
-	
-
 }(jQuery));
