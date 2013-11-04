af6da9dc264431ac96bbc41aa474a8ecd293edaf
SGupta14 <somya.gupta@innoraft.com>
2013-11-04 10:39:34 +0530

    disabling logo slide effect

--- docroot/sites/all/themes/ir_love/js/home.js
+++ docroot/sites/all/themes/ir_love/js/home.js
@@ -1,11 +1,11 @@
 (function ($) {
 	$(document).ready(function(){
 		if (screen.width > 769) {
-			ir_logo_effect();
+			//ir_logo_effect();
 			var slide_out = $('.ir-idea-font-icon');
 			var slide_in = $('.ir-idea-text');
 
-			$('.ir-main-idea').height(slide_in.height());
+			//$('.ir-main-idea').height(slide_in.height());
 			//$('.ir-idea-text').hide();
 		}
 	});
