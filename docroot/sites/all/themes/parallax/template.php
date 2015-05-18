f29076c49006f8f56682a7e4c7beb6382940d58f
Dinesh Sardiwal <dinesh.sardiwal@gmail.com>
2015-05-18 19:18:10 +0530

    Front page design work
    10120
    
    I have implementing new design .
    
    Part of the site affected

--- docroot/sites/all/themes/parallax/template.php
+++ docroot/sites/all/themes/parallax/template.php
@@ -4,3 +4,17 @@
  * @file
  * template.php
  */
+
+/**
+ *  
+ *
+ */
+
+function parallax_preprocess_html(&$vars) {
+  if ($vars['is_front']) {
+    drupal_add_css(drupal_get_path('theme','parallax').'/css/front.css');
+  }
+  else {
+    drupal_add_css(drupal_get_path('theme','parallax').'/css/general.css');
+  }
+}
