593ca37dae36cf448ccc2bfcd490cd1c86bae991
Dinesh Sardiwal <dinesh.sardiwal@gmail.com>
2015-05-21 10:34:08 +0530

    Completed first section css work on front page.
    Resf #10120
    
    i have completed first seciton block work on front page.

--- docroot/sites/all/themes/parallax/js/script.js
+++ docroot/sites/all/themes/parallax/js/script.js
@@ -23,27 +23,11 @@ Drupal.behaviors.my_custom_behavior = {
     
     // Window load event
     $( window ).load( function () {
-      $( '.intro .column-wrap' ).css( 'height', $( window ).height() );
-
-        //Pushing Content down to the height equal to Intro height + height necessary to finish animation
-       if ( intro.length > 0 ) {
-        content.css( 'margin-top', intro.height() * 1.5 );
-      }
       // Animation delay for intro features
       $( '.intro-features .icon-block' ).each( function () {
         var transDelay = $( this ).data( 'transition-delay' );
         $( this ).css( { 'transition-delay': transDelay + 'ms' } );
       });
-    });
-
-    // Window resize event
-    $( window ).resize( function () {
-      //Keeping Logo and Other-Logo at the bottom of teh page.
-      $( '.intro .column-wrap' ).css( 'height', $( window ).height() );
-      //Pushing Content down to the height equal to Intro section height + height necessary to finish animation.
-      if ( intro.length > 0 ) {
-        content.css( 'margin-top', intro.height() * 1.5 );
-      }
     });
 
     //Animating Intro Section
@@ -55,11 +39,20 @@ Drupal.behaviors.my_custom_behavior = {
       }
     });
 
-    jQuery(".navbar-toggle").click(function(){
-      $('.navbar').show();
-      $('.navbar-collapse').removeClass('collapse');
-      jQuery(".navbar").toggleClass("front-navbar-collapse-new");
-    }); 
+    jQuery(".navbar-toggle").click(function(e){
+      //$('.navbar').show();
+      //$('.navbar-collapse').removeClass('collapse');
+      jQuery(".main-navigations").addClass("front-navbar-collapse-new");
+      e.stopPropagation();
+    });
+
+    jQuery(document).click( function(e){
+      $element = jQuery(".main-navigations");
+      if ( $element.hasClass('front-navbar-collapse-new') ){
+        $element.removeClass('front-navbar-collapse-new');
+      }
+    } );
+
 });
 
     // Place your code here.
